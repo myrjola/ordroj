6b1ade3b86282efde64f9314f17f599a1a027873
Martin Yrjölä <martin.yrjola@gmail.com>
2023-04-09 08:53:33 +0300

    Footer WIP

--- README.md
+++ README.md
@@ -15,6 +15,4 @@ To create a production version of your app:
 pnpm build
 ```
 
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+You can preview the production build with `pnpm run preview`.
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,1 +1,25 @@
-<footer>Footer</footer>
+<script>
+    import {ExternalLink} from 'lucide-svelte'
+
+</script>
+
+<footer class="bg-gray-900" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="p-4 flex justify-between items-baseline">
+        <div class="text-xs leading-5 text-gray-400">&copy; 2023 Martin Yrjölä</div>
+        <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/myrjola/ordroj"
+                class="group rounded whitespace-nowrap text-gray-400 hover:text-white p-4 flex items-center gap-2 focus:outline-none focus:ring-2 focus:ring-white"
+        >
+            Forka mig på Github
+            <span class="flex items-center">
+          <ExternalLink
+              aria-label="Extern länk"
+              className="h-5 w-5 text-gray-400 group-hover:text-white"
+          />
+        </span>
+        </a>
+    </div>
+</footer>
